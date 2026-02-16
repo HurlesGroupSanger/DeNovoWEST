6cd7b8559804a5817341814526086a5275e76325
Erwan Delage <ed11@sanger.ac.uk>
2026-02-16 21:04:27 +0000

    fix : adjusted the combine_dne_dnn script to the new output format of the enrichment test, and used more explicit column names

--- misc/combine_dne_dnn.py
+++ misc/combine_dne_dnn.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
+import click
 import pandas as pd
 from scipy.stats import combine_pvalues
-import click
 
 # dne_file_all = (
 #     "../input/merged_all_dne_test_ppv_2020_03_09.tab"
@@ -36,51 +36,55 @@ def combine_dne_dnn(dne_file_all, dne_file_mis, dnn_file, out_file):
 
     # Load DNW results
     dne_all = pd.read_csv(dne_file_all, sep="\t")
-    dne_all.columns = [
-        "gene_id",
-        "expected_all",
-        "observed_all",
-        "dne_all_p",
-        "nb_sim_all",
-        "nb_mutations_stop_all",
-        "sequential_simulation_all",
-        "log_all",
-        "walltime_all",
-    ]
+    dne_all.columns = ["gene_id", "expected_all", "observed_all", "enrichment_all_pval"]
     dne_mis = pd.read_csv(dne_file_mis, sep="\t")
     dne_mis.columns = [
         "gene_id",
         "expected_mis",
         "observed_mis",
-        "dne_mis_p",
-        "nb_sim_mis",
-        "nb_mutations_stop_mis",
-        "sequential_simulation_mis",
-        "log_mis",
-        "walltime_mis",
+        "enrichment_mis_pval",
     ]
 
     # Load DNN results
     dnn = pd.read_csv(dnn_file, sep="\t")
     dnn = dnn[dnn["mutation_category"] == "missense"]
-    dnn.rename({"probability": "dnn_p"}, axis=1, inplace=True)
+    dnn.rename(
+        {"probability": "clustering_pval", "events_n": "nb_mis_variants", "dist": "clustering_dist"},
+        axis=1,
+        inplace=True,
+    )
 
     # Merge dataframes
     merged_data = pd.merge(dne_all, dnn, on="gene_id", how="left")
     merged_data = pd.merge(merged_data, dne_mis, on="gene_id", how="outer")
 
     # Combine p-values using Fisher's method (log-likelihood ratio test)
-    merged_data["com_p"] = merged_data[["dnn_p", "dne_mis_p"]].apply(
+    merged_data["combined_mis_pval"] = merged_data[["clustering_pval", "enrichment_mis_pval"]].apply(
         lambda pvals: combine_pvalues(pvals, method="fisher")[1], axis=1
     )
 
     # Take minimum of combined and enrichment p-values
-    merged_data["min_p"] = merged_data[["com_p", "dne_all_p"]].apply(
+    merged_data["min_pval"] = merged_data[["combined_mis_pval", "enrichment_all_pval"]].apply(
         lambda pvals: min(pvals.dropna(), default=1), axis=1
     )
 
     # Write to file
-    merged_data = merged_data[["gene_id", "gene_symbol", "min_p", "com_p", "dne_all_p", "dne_mis_p", "dnn_p"]]
+    merged_data = merged_data[
+        [
+            "gene_id",
+            "min_pval",
+            "combined_mis_pval",
+            "enrichment_all_pval",
+            "enrichment_mis_pval",
+            "clustering_pval",
+            "observed_all",
+            "expected_all",
+            "observed_mis",
+            "expected_mis",
+            "nb_mis_variants",
+            "clustering_dist",
+        ]
+    ]
     merged_data.to_csv(out_file, sep="\t", index=False)
 
 
